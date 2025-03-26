14dffdb00807743cb680fbe5dd683ab950af84ec
Yann11GAB <danieloubassou@gmail.com>
2025-03-26 21:50:51 +0100

    file

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter and setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency instance');
+    }
+    this._currency = value;
+  }
+
+  // Method to display full price format
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static conversion method
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both arguments must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
