b28cac238dae465714258eaf40769028280f8e94
Yann11GAB <danieloubassou@gmail.com>
2025-03-29 00:07:30 +0100

    file

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -3,7 +3,7 @@ export class HolbertonClass {
       this._year = year;
       this._location = location;
     }
-  
+
     get year() {
       return this._year;
     }
@@ -12,21 +12,21 @@ export class HolbertonClass {
       return this._location;
     }
   }
-  
+
   const class2019 = new HolbertonClass(2019, 'San Francisco');
   const class2020 = new HolbertonClass(2020, 'San Francisco');
-  
+
   export class StudentHolberton {
     constructor(firstName, lastName, holbertonClass) {
       this._firstName = firstName;
       this._lastName = lastName;
       this._holbertonClass = holbertonClass;
     }
-  
+
     get fullName() {
       return `${this._firstName} ${this._lastName}`;
     }
-  
+
     get holbertonClass() {
       return this._holbertonClass;
     }
@@ -34,7 +34,7 @@ export class HolbertonClass {
     get fullStudentDescription() {
       return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
     }
-}
+  }
 
   const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
   const student2 = new StudentHolberton('John', 'Doe', class2020);
@@ -42,4 +42,5 @@ export class HolbertonClass {
   const student4 = new StudentHolberton('Donald', 'Bush', class2019);
   const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-  export const listOfStudents = [student1, student2, student3, student4, student5];
+  const listOfStudents = [student1, student2, student3, student4, student5];
+  export default listOfStudents;
