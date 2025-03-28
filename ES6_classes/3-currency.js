e18e7d890dc1557bdc52dc5d1eb579d18595ed20
Yann11GAB <danieloubassou@gmail.com>
2025-03-29 00:38:52 +0100

    file

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,32 +1,49 @@
-export default class Currency {
+class Currency {
+  // Constructor for Currency
   constructor(code, name) {
-    this._name = name;
+    // Type validation for 'code'
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    // Type validation for 'name'
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    // Assigning values to private attributes
     this._code = code;
+    this._name = name;
   }
 
-  get name() {
-    return this._name;
-  }
-
+  // Getter and setter for 'code'
   get code() {
     return this._code;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('name must be an integer');
-    }
-    this._name = value;
-  }
-
   set code(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('code must be an string');
+      throw new TypeError('Code must be a string');
     }
     this._code = value;
   }
 
+  // Getter and setter for 'name'
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Method to display full currency information
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
+
+// Export the class
+export default Currency;
