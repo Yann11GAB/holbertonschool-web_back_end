2225588f0a18593c5f8ea63462f8a014b0a9ce6a
Yann11GAB <danieloubassou@gmail.com>
2025-04-02 12:52:39 +0200

    first project

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const v1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const v2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const v3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [v1, v2, v3];
+}
---
+++ ES6_data_manipulation/babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+  };
+// This configuration file is for Babel, a JavaScript compiler.
